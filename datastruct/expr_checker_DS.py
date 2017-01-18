d430deeb40942be38f07be66438117857f4f276a
Monish <monishmathpal@gmail.com>
2017-01-18 16:49:15 +0530

    Implementation of stack and list as a stack

---
+++ datastruct/expr_checker_DS.py
@@ -0,0 +1,25 @@
+import Stack_DS as sd
+
+def par(expr):
+    balanced = True
+    s = sd.Stack()
+    index = 0
+
+    while index < len(expr) and balanced:
+        sym = expr[index]
+
+        if sym == "(":
+            s.push(sym)
+        else:
+            if s.is_empty():
+                balanced == False
+            else:
+                s.pop()
+        index += 1
+
+    if balanced and s.is_empty():
+        return True
+    else:
+        return False
+
+print(par("(())"))   
