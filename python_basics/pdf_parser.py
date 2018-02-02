6f39c2fb3d704e5194bc2426941e46d1f3f04c03
Monish-Mathpal <monishmathpal@gmail.com>
2018-02-02 17:43:12 -0500

    added pdf_read function

--- python_basics/pdf_parser.py
+++ python_basics/pdf_parser.py
@@ -30,6 +30,51 @@ def frb_extract(page_raw_datapoints):
     return page_datapoints
 
 
+def gpl(rc_page_data):
+    reg_gpl = re.compile(rb"[Tf\n,Tw\n,TD\n](.*)Tj\n")
+    return reg_gpl.findall(rc_page_data)
+
+
+def acrobat(rc_page_data):
+    reg_acrobat = re.compile(rb"[Tf\n,Tw\n,TD\n](.*)Tj\n")
+    return reg_acrobat.findall(rc_page_data)
+
+
+def itext(rc_page_data):
+    reg_itext = re.compile(rb"[TD\n,Tf\n, T*\n](.*)Tj\n")
+    return reg_itext.findall(rc_page_data)
+
+
+def faceless(rc_page_data):
+    reg_itext = re.compile(rb"Td\n(.*)TJ\n")
+
+    return reg_itext.findall(rc_page_data)
+
+
+def pdf_read(pdfName, pagenum=0):
+    # pdb.set_trace()
+    with open(pdfName, 'rb') as pdfobj:
+        pdfReader = PyPDF2.PdfFileReader(pdfobj)
+        rc_page = pdfReader.getPage(pagenum)
+        rc_page_content = rc_page.getContents()
+        rc_page_data = rc_page_content.getData()
+        # print(rc_page_data)
+
+        if pdfReader.documentInfo['/Producer'].find('Acrobat Distiller')!= -1:
+            return acrobat(rc_page_data)
+
+        # elif pdfReader.documentInfo['/Producer'].find('GPL') != -1:
+        #     return gpl(rc_page_data)
+
+        elif pdfReader.documentInfo['/Producer'].find('iText') != -1:
+            return itext(rc_page_data)
+
+        elif pdfReader.documentInfo['/Producer'].find("faceless") != -1:
+            return faceless(rc_page_data)
+
+        else:
+            rc_page_data = rc_page.extractText()
+            return rc_page_data.split("\n")
 
 
 
